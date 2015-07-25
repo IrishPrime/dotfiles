8e63c999b218edf11da0b3570b722e1274daabd5
Michael O'Neill <irish.dot@gmail.com>
2015-07-24 22:39:05 -0400

    Replace pointless Python 2 template with Python 3 template

--- Templates/Templates/Python.py
+++ Templates/Templates/Python.py
@@ -1,1 +1,14 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
+""""""
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('file_path', metavar='FILE', type=str,
+            help='Path to input file')
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
