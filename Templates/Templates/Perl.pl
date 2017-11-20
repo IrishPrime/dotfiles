206bba7201d4fbd5db6732f2b4386f4e6f79f5f1
Michael O'Neill <irish.dot@gmail.com>
2017-11-19 23:21:04 -0500

    Add Europa customizations (#7)
    - Improve `perl` template.
    - Reformat `dunstrc`.
    - Set `keywordprg` for `perl` files to use `perldoc`.
    - Enable `perl` checker in `syntastic` for `vim`.
    - Resolves #6.

--- Templates/Templates/Perl.pl
+++ Templates/Templates/Perl.pl
@@ -1,7 +1,24 @@
 #!/usr/bin/perl
-# Michael O'Neill <irish.dot@gmail.com>
-# 
 
 use strict;
 use warnings;
+use Data::Dumper;
+use Getopt::Std;
+use Log::Message::Simple qw[msg error debug];
 
+use constant {
+	VERSION => '0.1.0',
+};
+
+$Getopt::Std::STANDARD_HELP_VERSION = 1;
+
+my %opts;
+
+getopts('', \%opts);
+
+sub HELP_MESSAGE {
+}
+
+sub VERSION_MESSAGE {
+	print "$0: v" . VERSION . "\n";
+}
